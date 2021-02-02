53f758303f0da84f6ca6517de6f390d021114b5f
Johnny Appleseed <39054009+lxvs@users.noreply.github.com>
2021-02-02 19:23:36 +0800

    Initial commit
    [N] Add ignore-error parameters to vim substitute.
    
    [N] Only process staged changes if any.
    
    Add a title and some minor improvements on
    choice of words in deployment batch.
    
    [d] Choose nothing is to choose deployment.
    
    [d] Improvements on help text.
    
    [d][j] Add last update info in deployment batch.
    
    [C][S] Improvements.
    
    [N] improvements on prompts
    
    [A][S] Add 'Amend Last Commit' and suppress the help info in Stash.
    
    [j] Improve the typesetting of jg.
    
    [N] Compare with 2nd last commit when there is no changes.
    
    Also fixed a typo: copyed/copied.
    
    [G] Add jggrepcommit, and improve jg typesettings.
    
    [d] add a pause after sucessfully deploy, and replace all 'echo.' with 'echo;'
    
    [N] (Improvemtns) Change '\' to '\' because Shell will consume half escapes.
    
    [d] replace all 'echo;...' with 'echo ...'

---
+++ deploy.bat
@@ -0,0 +1,89 @@
+@echo off
+setlocal
+title Johnny's Git Kit Deployment
+pushd %~dp0
+echo  Johnny's Git Kit Deployment
+echo;
+if not exist bin/jg (echo ^> Error: file bin/jg not found!) & (echo;) & goto End
+for /f "skip=1" %%i in (bin/jg) do if not defined lastupdt set lastupdt=%%i
+if defined lastupdt echo Last updated: %lastupdt:~1%
+echo;
+echo ^> Please choose what to do:
+echo   ^| 1     Deploy Johnny's Git Kit
+echo   ^| 2     Show help
+echo   ^| 0     Remove Johnny's Git Kit
+:Choose
+echo;
+set /p=^> Please select a number: <NUL
+set selection=
+set /p selection=
+if "%selection%" EQU "" goto Deploy
+if "%selection%" EQU "1" goto Deploy
+if "%selection%" EQU "0" goto Remove
+if "%selection%" EQU "2" call:ShowHelp & goto Choose
+echo ^> Invalid input.
+goto Choose
+:Deploy
+if not exist "bin" (
+    echo Error: Could not find the script folder.
+    goto End
+)
+if not exist "%userprofile%\bin\" (
+    md "%userprofile%\bin\"
+    echo ^> Created folder '%userprofile%\bin\'
+)
+for /r bin\ %%f in ( jg* ) do (( copy /Y "%%f" "%userprofile%\bin\" ) || goto SomethingIsWrong )
+call:Fin 1
+goto End
+:Remove
+if not exist "%userprofile%\bin\" (
+    echo ^> It hasn't deployed in your PC.
+    goto Choose
+)
+for /r bin\ %%f in ( jg* ) do ( del /Q "%userprofile%\bin\%%~nf" || goto SomethingIsWrong )
+rd "%userprofile%\bin" 2>NUL
+call:Fin 0
+goto End
+:Fin
+echo;
+if "%1" EQU "1" (
+    echo ^> Deployment finished.
+    echo;
+    pause
+    call:ShowHelp
+    goto:eof
+)
+if "%1" EQU "0" (
+    echo ^> Removement finished.
+    goto:eof
+)
+echo ^> Warning 330: an unreachable place is reached with parameter '%1'.
+goto:eof
+:ShowHelp
+echo;
+echo - HELP -
+echo;
+echo  ^| https://github.com/lxvs/jg
+echo  ^|
+echo  ^| It adds several more convenvient commands to Git Bash.
+echo  ^|
+echo  ^| Open Git Bash and use command 'jg' for more details.
+echo  ^|
+echo  ^|
+echo  ^| The term Johnny Git is come from 'jg', rather than otherwise.
+echo  ^|
+echo  ^| 'jg' was chosen as the prefix of all these commands because
+echo  ^| there is hardly any built-in command that initials with 'jg'.
+echo  ^|
+echo  ^| Besides, all these commands has the unique initial after 'jg'.
+echo  ^|
+echo  ^| Therefore, for example, you can simply type 'jgc' and
+echo  ^| press [Tab] to get command 'jgcommitnumber'.
+echo  ^|
+echo  ^| Thanks for using!
+echo;
+goto:eof
+:SomethingIsWrong
+echo Something is wrong!
+:End
+pause & goto:eof
