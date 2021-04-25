b713df5d72459f59318e14b055c1680d37645fff
lxvs <jn.apsd@gmail.com>
2021-04-25 15:31:02 +0800

    add deploy.sh for BASH systems

---
+++ deploy.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+ver=$(cat VERSION 2>/dev/null)
+[[ -z $ver ]] && echo "Warning: failed to get version"
+pushd $(dirname $0) 1>/dev/null
+echo "Johnny's Git Kit $ver Deployment"
+echo "https://github.com/lxvs/jg"
+echo ""
+
+script_name="./$(basename $0)"
+target_dir="/usr/local/bin"
+
+if [[ $# -eq 1 ]]; then
+    if [[ "$1" == "1" || "$1" == "deploy" ]]; then
+        if [[ ! -d $target_dir ]]; then
+            echo "ERROR: $target_dir does not exist or is not a directory!" >&2
+            exit 101
+        fi
+
+        echo "Copying..."
+
+        for jgfile in ./bin/jg*
+        do
+            cp $jgfile $target_dir || exit 102
+            chmod +x $target_dir/$(basename $jgfile)
+        done
+
+        if [[ ! -z $ver ]]; then
+            echo "#!/bin/bash" > $target_dir/jgversion
+            echo "echo \"Johnny's Git Kit $ver\"" >> $target_dir/jgversion
+            echo "echo \"https://github.com/lxvs/jg\"" >> $target_dir/jgversion
+        fi
+
+        chmod +x $target_dir/jgversion
+
+        echo "Deployment finished"
+        exit 0
+
+    elif [[ "$1" == "0" || "$1" == "remove" ]]; then
+
+        if ! compgen -G "$target_dir/jg*" >/dev/null; then
+            echo "ERROR: there is no Johnny's Git Kit deployed" >&2
+            exit 2
+        fi
+
+        echo "Removing deprecated commands..."
+        rm -f "$target_dir/jg" || exit 103
+        rm -f "$target_dir/jgstash" || exit 103
+
+        echo "Removing Johnny's Git Kit..."
+
+        for jgfile in ./bin/jg*
+        do
+            rm -f $target_dir/$(basename $jgfile) || exit 103
+        done
+        rm -f $target_dir/jgversion || exit 103
+
+        echo "Removal finished"
+        exit 0
+
+    else
+        echo "invalid option: $1" >&2
+        echo "To deploy, use command 'sudo $script_name 1' or 'sudo $script_name deploy'" >&2
+        echo "To remove, use command 'sudo $script_name 0' or 'sudo $script_name remove'" >&2
+        exit 1
+    fi
+elif [[ $# -eq 0 ]]; then
+    echo "To deploy, use command 'sudo $script_name 1' or 'sudo $script_name deploy'"
+    echo "To remove, use command 'sudo $script_name 0' or 'sudo $script_name remove'"
+    exit 0
+else
+    echo "invalid option: $@" >&2
+    echo "To deploy, use command 'sudo $script_name 1' or 'sudo $script_name deploy'" >&2
+    echo "To remove, use command 'sudo $script_name 0' or 'sudo $script_name remove'" >&2
+    exit 1
+fi
