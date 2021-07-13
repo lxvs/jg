0b990c74ad1e8d4f8c9e7a6a00ce546483740857
lxvs <jn.apsd@gmail.com>
2021-07-13 16:56:38 +0800

    Improve deploy.bat
    Improve CLI mode.
    Add usage.
    Add variable name and link.

--- deploy.bat
+++ deploy.bat
@@ -2,29 +2,49 @@
 setlocal
 set rev=
 if exist VERSION for /f %%i in (VERSION) do if not defined rev set "rev=%%i"
-title Johnny's Git Kit v%rev% Deployment
+set "name=Johnny's Git Kit"
+set "link=https://github.com/lxvs/jg"
+title %name% v%rev% Deployment
 pushd %~dp0
 
 set "climode="
 if not "%~1" == "" (
+    set "climode=1"
     if "%~2" == "" (
-        if "%~1%" == "1" (
-            set "climode=1"
+        if "%~1" == "/?" (
+            call:Logo
+            call:Usage
+            exit /b
+        ) else if "%~1%" == "1" (
             goto deploy
         ) else if "%~1%" == "0" (
-            set "climode=1"
             goto remove
-        ) else >&2 echo warning: ignored invalid argument: %~1
-    ) else >&2 echo warning: too many arguments, ignoring all
+        ) else if /i "%~1%" == "install" (
+            goto deploy
+        ) else if /i "%~1%" == "uninstall" (
+            goto remove
+        ) else if /i "%~1%" == "deploy" (
+            goto deploy
+        ) else if /i "%~1%" == "remove" (
+            goto remove
+        ) else (
+            >&2 echo ERROR: ignored invalid argument: %~1
+            >&2 echo;
+            >&2 call:Usage
+            exit /b 1
+        )
+    ) else (
+        >&2 echo ERROR: too many arguments.
+        >&2 echo;
+        >&2 call:Usage
+        exit /b 1
+    )
 )
 
-@echo;
-@echo     Johnny's Git Kit v%rev% Deployment
-@echo     https://github.com/lxvs/jg
-@echo;
+call:Logo
 @echo Please choose what to do:
-@echo   1  Deploy Johnny's Git Kit
-@echo   0  Remove Johnny's Git Kit
+@echo   1  Deploy %name%
+@echo   0  Remove %name%
 
 :choose
 @echo;
@@ -46,24 +66,24 @@ if not exist "%USERPROFILE%\bin\" (
     md "%USERPROFILE%\bin\"
     @echo Created folder '%USERPROFILE%\bin\'
 ) else call:RmDep
-@echo Copying Johnny's Git Kit to your PC...
+@echo Copying %name% to your PC...
 for /r bin\ %%f in (jg*) do copy /Y "%%f" "%USERPROFILE%\bin\"
 if defined rev (
     echo #!/bin/bash
-    echo echo "Johnny's Git Kit v%rev%"
-    echo echo "https://github.com/lxvs/jg"
+    echo echo "%name% v%rev%"
+    echo echo "%link%"
 )>%USERPROFILE%\bin\jgversion || >&2 echo Warning: failed to create jgversion
 call:Fin 1
 exit /b
 
 :remove
 if not exist "%USERPROFILE%\bin\" (
-    >&2 echo There is no Johnny's Git Kit deployed in your PC.
+    >&2 echo There is no %name% deployed in your PC.
     if not defined climode pause
     exit /b 1
 )
 call:RmDep
->&2 echo Removing Johnny's Git Kit from your PC...
+>&2 echo Removing %name% from your PC...
 for /r bin\ %%f in (jg*) do del "%USERPROFILE%\bin\%%~nf" 2>NUL
 del "%USERPROFILE%\bin\jgversion" 2>NUL
 rd "%USERPROFILE%\bin" 2>NUL
@@ -86,4 +106,17 @@ exit /b 0
 del "%USERPROFILE%\bin\jg" 2>NUL
 del "%USERPROFILE%\bin\jgjustpullit" 2>NUL
 del "%USERPROFILE%\bin\jgstash" 2>NUL
+exit /b
+
+:Logo
+@echo;
+@echo     %name% v%rev% Deployment
+@echo     %link%
+@echo;
+exit /b
+
+:Usage
+@echo Usage:
+@echo     deploy.bat [ 1 ^| install ^| deploy ]
+@echo     deploy.bat [ 0 ^| uninstall ^| remove ]
 exit /b
