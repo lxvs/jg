b25c907ff4a251996b26bb13a1654894ba1b75a1
lxvs <jn.apsd@gmail.com>
2021-07-13 17:13:49 +0800

    Improve deploy.sh
    Replace 'if's with 'case'.
    Replace '[[ ]]'s with '[ ]'s.
    Add argument -?, -h and --help.
    Add variable 'name' and 'link'.

--- deploy.sh
+++ deploy.sh
@@ -1,79 +1,98 @@
 #!/bin/bash
 
-rev=$(cat VERSION 2>/dev/null)
-[[ -z $rev ]] && echo "Warning: failed to get version"
-pushd $(dirname $0) 1>/dev/null
-echo "Johnny's Git Kit $rev Deployment"
-echo "https://github.com/lxvs/jg"
-echo ""
+Logo(){
+    echo "
+    $name $rev Deployment
+    $link"
+}
 
+Usage(){
+    echo "
+Usage:
+
+Install with one of below command:
+    $script_name 1
+    $script_name install
+    $script_name deploy
+
+Uninstall:
+    $script_name 0
+    $script_name uninstall
+    $script_name remove"
+}
+
+declare -r name="Johnny's Git Kit"
+declare -r link="https://github.com/lxvs/jg"
+rev=$(cat VERSION 2>/dev/null)
+[ -z $rev ] && echo "Warning: failed to get version" >&2
+pushd $(dirname $0) 1>/dev/null
 script_name="./$(basename $0)"
-if [ "${OS:0:7}" == "Windows" ]; then
+if [ "${OS:0:7}" == "Windows" ]
+then
     target_dir="/c/Users/$USERNAME/bin"
 else
     target_dir="/usr/local/bin"
 fi
 
-if [[ $# -eq 1 ]]; then
-    if [[ "$1" == "1" || "$1" == "deploy" ]]; then
-        if [[ ! -d $target_dir ]]; then
-            mkdir $target_dir
-        fi
+if [ $# -eq 1 ]; then
+    case "$1" in
+        "-?"|"-h"|"--help")
+            Logo
+            Usage
+            exit
+            ;;
 
-        echo "Copying..."
+        "1"|"install"|"deploy")
+            [ -d $target_dir ] || mkdir $target_dir
+            echo "Copying..."
+            for jgfile in ./bin/jg*
+            do
+                cp $jgfile $target_dir || exit 1
+                chmod +x $target_dir/$(basename $jgfile)
+            done
+            if [ ! -z $rev ]
+            then
+                echo "
+#!/bin/bash
+echo '$name $rev'
+echo '$link'" > $target_dir/jgversion
+            fi
+            chmod +x $target_dir/jgversion
+            echo "Deployment finished"
+            exit 0
+            ;;
 
-        for jgfile in ./bin/jg*
-        do
-            cp $jgfile $target_dir || exit 102
-            chmod +x $target_dir/$(basename $jgfile)
-        done
+        "0"|"uninstall"|"remove")
+            if ! compgen -G "$target_dir/jg*" >/dev/null
+            then
+                echo "ERROR: there is no $name deployed." >&2
+                exit 1
+            fi
+            echo "Removing deprecated commands..."
+            rm -f "$target_dir/jg"
+            rm -f "$target_dir/jgstash"
+            echo "Removing $name..."
+            for jgfile in ./bin/jg*
+            do
+                rm -f $target_dir/$(basename $jgfile)
+            done
+            rm -f $target_dir/jgversion
+            [ $? == 0 ] && echo "Complete."
+            exit
+            ;;
+        *)
+            >&2 echo "ERROR: Invalid option: $1"
+            >&2 Usage
+            exit 1
+            ;;
+    esac
 
-        if [[ ! -z $rev ]]; then
-            echo "#!/bin/bash" > $target_dir/jgversion
-            echo "echo \"Johnny's Git Kit $rev\"" >> $target_dir/jgversion
-            echo "echo \"https://github.com/lxvs/jg\"" >> $target_dir/jgversion
-        fi
-
-        chmod +x $target_dir/jgversion
-
-        echo "Deployment finished"
-        exit 0
-
-    elif [[ "$1" == "0" || "$1" == "remove" ]]; then
-
-        if ! compgen -G "$target_dir/jg*" >/dev/null; then
-            echo "ERROR: there is no Johnny's Git Kit deployed" >&2
-            exit 2
-        fi
-
-        echo "Removing deprecated commands..."
-        rm -f "$target_dir/jg" || exit 103
-        rm -f "$target_dir/jgstash" || exit 103
-
-        echo "Removing Johnny's Git Kit..."
-
-        for jgfile in ./bin/jg*
-        do
-            rm -f $target_dir/$(basename $jgfile) || exit 103
-        done
-        rm -f $target_dir/jgversion || exit 103
-
-        echo "Removal finished"
-        exit 0
-
-    else
-        echo "invalid option: $1" >&2
-        echo "To deploy, use command 'sudo $script_name 1' or 'sudo $script_name deploy'" >&2
-        echo "To remove, use command 'sudo $script_name 0' or 'sudo $script_name remove'" >&2
-        exit 1
-    fi
-elif [[ $# -eq 0 ]]; then
-    echo "To deploy, use command 'sudo $script_name 1' or 'sudo $script_name deploy'"
-    echo "To remove, use command 'sudo $script_name 0' or 'sudo $script_name remove'"
+elif [ $# == 0 ]; then
+    Logo
+    Usage
     exit 0
 else
-    echo "invalid option: $@" >&2
-    echo "To deploy, use command 'sudo $script_name 1' or 'sudo $script_name deploy'" >&2
-    echo "To remove, use command 'sudo $script_name 0' or 'sudo $script_name remove'" >&2
+    >&2 echo "ERROR: Too many arguments."
+    >&2 Usage
     exit 1
 fi
