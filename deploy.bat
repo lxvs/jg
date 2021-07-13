b7cebf2eed79cc4fb98fc84d8d3734c145b97200
lxvs <jn.apsd@gmail.com>
2021-07-13 16:32:16 +0800

    Add echo off, remove '@'s and remove some error prompts

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,12 @@
-@setlocal
-@set rev=
-@if exist VERSION @for /f %%i in (VERSION) do @if not defined rev set "rev=%%i"
-@title Johnny's Git Kit v%rev% Deployment
-@pushd %~dp0
+@echo off
+setlocal
+set rev=
+if exist VERSION for /f %%i in (VERSION) do if not defined rev set "rev=%%i"
+title Johnny's Git Kit v%rev% Deployment
+pushd %~dp0
 
-@set "climode="
-@if not "%~1" == "" (
+set "climode="
+if not "%~1" == "" (
     if "%~2" == "" (
         if "%~1%" == "1" (
             set "climode=1"
@@ -13,9 +14,10 @@
         ) else if "%~1%" == "0" (
             set "climode=1"
             goto remove
-        ) else @echo warning: ignored invalid argument: %~1
-    ) else @echo warning: too many arguments, ignoring all
+        ) else >&2 echo warning: ignored invalid argument: %~1
+    ) else >&2 echo warning: too many arguments, ignoring all
 )
+
 @echo Johnny's Git Kit v%rev% Deployment
 @echo https://github.com/lxvs/jg
 @echo;
@@ -25,70 +27,62 @@
 
 :choose
 @echo;
-@set /p=Please select a number: <NUL
-@set selection=
-@set /p selection=
-@if "%selection%" == "1" goto deploy
-@if "%selection%" == "0" goto remove
-@echo Invalid input.
-@goto choose
+set /p=Please select a number: <NUL
+set selection=
+set /p selection=
+if "%selection%" == "1" goto deploy
+if "%selection%" == "0" goto remove
+>&2 echo Invalid input.
+goto choose
 
 :deploy
-@if not exist "bin" (
-    @echo Error: Could not find the script folder.
-    @if not defined climode @pause
+if not exist "bin" (
+    >&2 echo Error: Could not find the script folder.
+    if not defined climode pause
     exit /b 1
 )
-@if not exist "%USERPROFILE%\bin\" (
+if not exist "%USERPROFILE%\bin\" (
     md "%USERPROFILE%\bin\"
     @echo Created folder '%USERPROFILE%\bin\'
-) else @call:RmDep
+) else call:RmDep
 @echo Copying Johnny's Git Kit to your PC...
-@for /r bin\ %%f in ( jg* ) do @( copy /Y "%%f" "%USERPROFILE%\bin\" || @goto somethingIsWrong )
-@if defined rev @(
+for /r bin\ %%f in (jg*) do copy /Y "%%f" "%USERPROFILE%\bin\"
+if defined rev (
     echo #!/bin/bash
     echo echo "Johnny's Git Kit v%rev%"
     echo echo "https://github.com/lxvs/jg"
-)>%USERPROFILE%\bin\jgversion || @echo Warning: failed to create jgversion
-@call:Fin 1
-@exit /b
+)>%USERPROFILE%\bin\jgversion || >&2 echo Warning: failed to create jgversion
+call:Fin 1
+exit /b
 
 :remove
-@if not exist "%USERPROFILE%\bin\" (
-    @echo There is no Johnny's Git Kit deployed in your PC.
-    @if not defined climode @pause
-    exit /b 2
+if not exist "%USERPROFILE%\bin\" (
+    >&2 echo There is no Johnny's Git Kit deployed in your PC.
+    if not defined climode pause
+    exit /b 1
 )
-@call:RmDep
-@echo Removing Johnny's Git Kit from your PC...
-@for /r bin\ %%f in ( jg* ) do @( del "%USERPROFILE%\bin\%%~nf" 2>NUL || @goto somethingIsWrong )
-@del "%USERPROFILE%\bin\jgversion" 2>NUL
-@rd "%USERPROFILE%\bin" 2>NUL
-@call:Fin 0
-@exit /b
+call:RmDep
+>&2 echo Removing Johnny's Git Kit from your PC...
+for /r bin\ %%f in (jg*) do del "%USERPROFILE%\bin\%%~nf" 2>NUL
+del "%USERPROFILE%\bin\jgversion" 2>NUL
+rd "%USERPROFILE%\bin" 2>NUL
+call:Fin 0
+exit /b
 
 :Fin
 @echo;
-@if "%~1" == "1" (
+if "%~1" == "1" (
     @echo Deployment finished.
 ) else if "%~1" == "0" (
     @echo Removal finished.
-) else (
-    @echo warning 600: unspecified operation
-    @if not defined climode @pause
-    exit /b 600
 )
-@if not defined climode @pause
-@exit /b 0
+if not defined climode pause
+exit /b 0
 
 :RmDep
 @echo;
 @echo Removing deprecated commands...
-@del "%USERPROFILE%\bin\jg" 2>NUL
-@del "%USERPROFILE%\bin\jgjustpullit" 2>NUL
-@del "%USERPROFILE%\bin\jgstash" 2>NUL
-@exit /b
-
-:somethingIsWrong
-@echo Something is wrong!
-@exit /b 999
+del "%USERPROFILE%\bin\jg" 2>NUL
+del "%USERPROFILE%\bin\jgjustpullit" 2>NUL
+del "%USERPROFILE%\bin\jgstash" 2>NUL
+exit /b
